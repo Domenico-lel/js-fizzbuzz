d90b48605767a6401df4616e58a03190fda1b1e7
Domenico-lel <domenicolella2@gmail.com>
2023-11-21 17:46:30 +0100

    da identificvare qualche passaggio

--- boot.js
+++ boot.js
@@ -1,16 +1,23 @@
 // selezioniamo il container
 const container = document.querySelector(".container");
 
-
+// generiamo i numeri da 1 a 100
 for(let i =  1; i <= 100; i++){
-
     // creiamo il div generico
     const square = document.createElement("div");
     // aggiungiamo la classe "square" al div
-    square.classList.add("square");
+    square.classList.add("square"); 
     // aggiungere il contenuto al div
     square.append(i);
     // aggiungere il div al container
     container.append(square)
-    
+    if (i % 3 === 0 && i % 5 === 0) {
+        document.write('<p class="fizzbuzz">FizzBuzz</p>');
+    } else if (i % 3 === 0) {
+        document.write('<p class="fizz">Fizz</p>');
+    } else if (i % 5 === 0) {
+        document.write('<p class="buzz">Buzz</p>');
+    } else {
+        document.write('<p class="normal">' + i + '</p>');
+    }
 }
